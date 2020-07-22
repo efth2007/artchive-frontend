a4720d1422c6918508350a1952b8bbea095cce21
efth2007 <efth2007@hotmail.com>
2020-07-22 15:38:33 +0200

    Basic navigation

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+
+export default function Navigation() {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand as={NavLink} to="/">
+        YOUR PROJECT NAME
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav style={{ width: "100%" }} fill>
+          <Nav.Item>
+            <Nav.Link exact as={NavLink} to="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/other">
+              Other
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/signup">
+              Sign up
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/login">
+              Login
+            </Nav.Link>
+          </Nav.Item>
+
+          <Button>Logout</Button>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
