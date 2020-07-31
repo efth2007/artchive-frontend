034f6906ac2283343ab01b3c3b9782fd6a3f4fa6
efth2007 <efth2007@hotmail.com>
2020-07-31 10:42:34 +0200

    Style homepage

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFeaturedArtist } from "../../store/featuredArtist/actions";
 import { selectFeaturedArtist } from "../../store/featuredArtist/selectors";
+
 //import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
@@ -44,39 +45,64 @@ export default function Homepage() {
     <div>
       <div class="jumbotron">
         <h1>Welcome to Ar(t)chive</h1>
-        <h3>BLABALBLABLAB</h3>
+        <h4>
+          BLabalbablaba
+          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. */}
+        </h4>
       </div>
-      <div class="card">
-        <h4>Today's featured artist: </h4>
-        {!featuredArtist ? null : (
-          <div>
-            {featuredArtist.knownAs ? (
-              <h3>{featuredArtist.knownAs}</h3>
-            ) : (
-              <h3>
-                {featuredArtist.firstName} {featuredArtist.lastName}{" "}
-              </h3>
-            )}
+      <div class="container">
+        <div class="card text-left border rounded">
+          <div class="card-header">Featured artist of the day: </div>
+          {!featuredArtist ? null : (
+            <div class="card-body">
+              <div class="row">
+                <div class="col-4">
+                  <img src={featuredArtist.imageUrl} alt="new" height="300" />
+                </div>
+                <div class="col-7">
+                  <Link to={`/artists/${featuredArtist.id}`}>
+                    {" "}
+                    {featuredArtist.knownAs ? (
+                      <h3>{featuredArtist.knownAs}</h3>
+                    ) : (
+                      <h3>
+                        {featuredArtist.firstName} {featuredArtist.lastName}{" "}
+                      </h3>
+                    )}
+                  </Link>
+                  <p>Nationality: {featuredArtist.nationality}</p>
+                  <p>
+                    * {featuredArtist.placeOfBirth}, {featuredArtist.bornOn}
+                    <br />† {featuredArtist.placeOfDeath},{" "}
+                    {featuredArtist.diedOn}
+                  </p>
+                  <p>
+                    Tags:{" "}
+                    {!featuredArtist.tags
+                      ? null
+                      : featuredArtist.tags.map((t, index) => (
+                          <span key={t.id}>
+                            {(index ? ", " : "") + t.tagName}
+                          </span>
+                        ))}
+                  </p>
 
-            <p>
-              {featuredArtist.placeOfBirth}, {featuredArtist.bornOn} -{" "}
-              {featuredArtist.placeOfDeath}, {featuredArtist.diedOn}
-            </p>
-            <p>
-              Tags:{" "}
-              {!featuredArtist.tags
-                ? null
-                : featuredArtist.tags.map((t, index) => (
-                    <span key={t.id}>{(index ? ", " : "") + t.tagName}</span>
-                  ))}
-            </p>
-
-            <p>
-              <a href={featuredArtist.wikiUrl}>View wiki page...</a>
-              <br />
-            </p>
-          </div>
-        )}
+                  <p>{featuredArtist.bio}</p>
+                  <p>
+                    <a href={featuredArtist.wikiUrl}>View wiki page...</a>
+                    <br />
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
